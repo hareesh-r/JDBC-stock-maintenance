2bc5020512c7b87b310088742319fab4140a10d0
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-15 18:59:08 +0530

    feat : Added Jar file and SQL create file

---
+++ Req SQL tables/stockdb.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE `stock` ;
+
+CREATE TABLE `customer` (
+  `idcustomer` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phoneno` varchar(255) NOT NULL,
+  PRIMARY KEY (`idcustomer`),
+  UNIQUE KEY `idcustomer_UNIQUE` (`idcustomer`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+CREATE TABLE `order` (
+  `orderid` int NOT NULL AUTO_INCREMENT,
+  `quantity` int NOT NULL,
+  `item` varchar(255) NOT NULL,
+  PRIMARY KEY (`orderid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
