95c1f67c50aca108fd766e9f40f097f7c43d2233
hareesh-r <hareeshprogrammer@gmail.com>
2021-12-23 19:17:16 +0530

    feat : Added GUI boilerplate

--- src/MySQLConnection/MySqlDMLCommands.java
+++ src/MySQLConnection/MySqlDMLCommands.java
@@ -1,8 +1,10 @@
 package MySQLConnection;
-
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 
-public class MySqlDMLCommands {
+public class MySqlDMLCommands implements ActionListener{
 
     public static class Customer {
 
@@ -131,28 +133,98 @@ public class MySqlDMLCommands {
         }
     }
 
+    public static class Invoice {
+
+        String connectionName,connectionPassword;
+
+        Invoice(String connectionName,String connectionPassword){
+            this.connectionName = connectionName;
+            this.connectionPassword = connectionPassword;
+        }
+
+        private void generate(int id){
+
+        }
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String usernameText = userName.getText();
+        String passwordText = password.getText();
+        if(usernameText.equals("hareesh") && passwordText.equals("hareeshtheboss")){
+            successLabel.setText("Admin Logged in!");
+        }else{
+            successLabel.setText("User Logged in!");
+        }
+    }
+    private static JFrame frame;
+    private static JPanel panel;
+    private static JLabel userLabel, passwordLabel,successLabel;
+    private static JTextField userName;
+    private static JPasswordField password;
+    private static JButton loginButton;
 
     public static void main(String[] args) throws SQLException {
-        String connectionName = "root";
-        String connectionPassword = "password";
 
-        Customer cust = new Customer(connectionName,connectionPassword);
-        cust.insert(7,"Hareesh Jr","Somewhere under the sky","9988776655","customer");
-        cust.update(5,"Hareesh Sr","Somewhere under the sky","9988776655","customer");
-        cust.delete(5,"customer");
-        cust.display("customer");
+        frame = new JFrame("Stock Maintenance");
+        panel = new JPanel();
+        frame.setSize(800,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
+
+        userName = new JTextField(100);
+        userName.setBounds(100,20,160,25);
+        panel.add(userName);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        password = new JPasswordField(100);
+        password.setBounds(100,50,160,25);
+        panel.add(password);
+
+        loginButton = new JButton("Login");
+        loginButton.setBounds(170,80,90,25);
+        loginButton.addActionListener((ActionListener) new MySqlDMLCommands());
+        panel.add(loginButton);
+
+        successLabel = new JLabel("");
+        successLabel.setBounds(10,100,180,25);
+        panel.add(successLabel);
 
 
-        Order order = new Order(connectionName,connectionPassword);
-        order.place(4,4,"Bottles");
-        order.place(5,3,"Phones");
-        order.place(6,7,"Laptops");
-        order.cancel(4);
-        order.display();
 
-        Product product = new Product(connectionName,connectionPassword);
-        product.add(2,"Bag",1000,"Chennai");
-        product.update(2,"Bags",1100,"Chennai");
-        product.delete(2);
+
+//        String connectionName = "root";
+//        String connectionPassword = "password";
+//
+//        Customer cust = new Customer(connectionName,connectionPassword);
+//        cust.insert(7,"Hemesh","RMKEC","9988776650","customer");
+//        cust.update(5,"Hareesh Sr","Somewhere under the sky","9988776655","customer");
+//        cust.delete(5,"customer");
+//        cust.display("customer");
+//
+//
+//        Order order = new Order(connectionName,connectionPassword);
+//        order.place(4,4,"Bottles");
+//        order.place(5,3,"Phones");
+//        order.place(6,7,"Laptops");
+//        order.cancel(4);
+//        order.display();
+//
+//        Product product = new Product(connectionName,connectionPassword);
+//        product.add(2,"Bag",1000,"Chennai");
+//        product.update(2,"Bags",1100,"Chennai");
+//        product.delete(2);
     }
 }
